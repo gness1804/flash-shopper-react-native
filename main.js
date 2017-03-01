07b731704e79ae33da0305040d77b3ea70c37fb8
gness1804 <graham.nessler@gmail.com>
2017-02-28 22:27:58 -0700

    Break main.js styles into separate file

--- main.js
+++ main.js
@@ -8,6 +8,7 @@ import {
   Button,
   ScrollView,
 } from 'react-native';
+import styles from './styles/main-styles'
 
 class Main extends Component {
   constructor(props){
@@ -136,34 +137,5 @@ class Main extends Component {
   }
 
 }
-
-const styles = StyleSheet.create({
-  eachItemContainer: {
-    backgroundColor: '#ffffff',
-    borderColor: 'black',
-    borderWidth: 1,
-    marginTop: 20,
-  },
-  inputField: {
-    borderColor: 'black',
-    borderWidth: 1,
-    height: 30,
-    margin: 10,
-    padding: 10,
-    width: 250,
-  },
-  greenButtons: {
-    color: 'rgb(160, 236, 175);',
-    marginTop: 10,
-  },
-  redButtons: {
-    color: 'rgb(255, 0, 0);',
-    marginTop: 10,
-  },
-  text: {
-    marginTop: 10,
-    textAlign: 'center',
-  }
-})
 
 export default Main;
